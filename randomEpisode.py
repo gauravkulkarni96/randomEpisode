f3df3d46be5818938de9293b1823f79c5278578b
gauravkulkarni96 <gauravkulkarni96@gmail.com>
2018-06-09 20:23:17 +0530

    multi season support

--- randomEpisode.py
+++ randomEpisode.py
@@ -1,47 +1,65 @@
 import os
 import random
+import sys
 
 RECENT_LENGTH = 10
-path = "/home/gaurav/Documents/Friends [chromium]"
-seasons = os.listdir(path)
-episodes = []
-seasonPath = ""
-episodePath = ""
-print "er"
-while True:
+
+TV_SERIES_PATHS = {
+	"friends" : "/home/gaurav/Documents/Friends [chromium]"
+	}
+
+def selectEpisode(path):
+	seasons = os.listdir(path)
+	episodes = []
+	seasonPath = ""
+	episodePath = ""
+	print "er"
 	while True:
-		season = random.choice(seasons)
-		seasonPath = path+"/"+season
-		if os.path.isdir(seasonPath):
-			episodes = os.listdir(seasonPath)
+		while True:
+			season = random.choice(seasons)
+			seasonPath = path+"/"+season
+			if os.path.isdir(seasonPath):
+				episodes = os.listdir(seasonPath)
+				break
+
+		while True:
+			episode = random.choice(episodes)
+			extIndex = episode.rfind(".")
+			extension = episode[extIndex:]
+			episodePath = seasonPath+"/"+episode
+			if extension in [".mkv", ".mp4", ".hvec"]:
+				print episode
+				break
+		try:
+			series = path.split("/")[-1]
+			recents = open(os.path.expanduser("~")+"/.recents"+series, "r")
+			recent_episodes = recents.readlines()
+		except:
+			recent_episodes = []
+		# print len(recent_episodes)
+		if episodePath not in recent_episodes:
+			if len(recent_episodes) >= RECENT_LENGTH:
+				recent_episodes = recent_episodes[1:]
+			recent_episodes.append(episodePath+"\n")
+			recents = open(os.path.expanduser("~")+"/.recents"+series, "w")
+			recents.writelines(recent_episodes)
+			recents.close()
 			break
 
-	while True:
-		episode = random.choice(episodes)
-		extIndex = episode.rfind(".")
-		extension = episode[extIndex:]
-		episodePath = seasonPath+"/"+episode
-		if extension in [".mkv", ".mp4", ".hvec"]:
-			print episode
-			break
-	try:
-		# series = path.split("/")[-1]
-		recents = open(os.path.expanduser("~")+"/.recents", "r")
-		recent_episodes = recents.readlines()
-	except:
-		recent_episodes = []
-	# print len(recent_episodes)
-	if episodePath not in recent_episodes:
-		if len(recent_episodes) >= RECENT_LENGTH:
-			recent_episodes = recent_episodes[1:]
-		recent_episodes.append(episodePath+"\n")
-		recents = open(os.path.expanduser("~")+"/.recents", "w")
-		recents.writelines(recent_episodes)
-		recents.close()
-		break
+	episodePath = episodePath.replace("~", "\~")
+	episodePath = episodePath.replace("$", "\$")
+	episodePath = episodePath.replace(" ", "\\ ")
+	command = "vlc "+episodePath
+	os.system(command)
+	return
 
-episodePath = episodePath.replace("~", "\~")
-episodePath = episodePath.replace("$", "\$")
-episodePath = episodePath.replace(" ", "\\ ")
-command = "vlc "+episodePath
-os.system(command)
+if __name__ == "__main__":
+	series = ""
+	if len(sys.argv) == 1:
+		selectEpisode(TV_SERIES_PATHS["friends"])
+	else:
+		series = sys.argv[1]
+		if series.lower() in TV_SERIES_PATHS:
+			selectEpisode(TV_SERIES_PATHS[series.lower()])
+		else:
+			print "{} name does not exist in TV_SERIES_PATHS".format(series)
